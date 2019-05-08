76cdac9b43eeb35db972a79ed800657fa3a8dfd6
Niklas Voss <niklas.voss@gmail.com>
2019-05-08 12:18:37 +0200

    Second testcase using external call similar to production app

---
+++ testapp/build-and-push.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker build -t trevex/testapp:$1
+docker push trevex/testapp:$1
---
+++ testapp/main.go
@@ -0,0 +1,34 @@
+package main // import "github.com/trevex/linkerd-test/testapp"
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/imroc/req"
+)
+
+func test(c *gin.Context) {
+	r, err := req.Get("https://whatthecommit.com/index.txt")
+	if err != nil {
+		c.String(503, fmt.Sprintf("%v", err))
+		return
+	}
+	str, err := r.ToString()
+	if err != nil {
+		c.String(503, fmt.Sprintf("%v", err))
+		return
+	}
+	c.String(201, str)
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/test", test)
+	r.POST("/test", test)
+	r.PUT("/test", test)
+	r.DELETE("/test", test)
+	r.PATCH("/test", test)
+	r.HEAD("/test", test)
+	r.OPTIONS("/test", test)
+	r.Run()
+}
